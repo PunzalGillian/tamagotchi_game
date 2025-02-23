4ca7365d83a9dea75516f400a746a72f1e11cf56
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-02-24 01:07:47 +0800

    Added the tamagotchi shell

--- tamagotchi.py
+++ tamagotchi.py
@@ -1,10 +1,23 @@
+import pygame
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor, QPixmap, QPainterPath, QPen
+from PyQt5.QtCore import QRectF
+
+# load sfx
+pygame.mixer.init()
+hatch_sound =  pygame.mixer.Sound("sfx/hatching.wav")
+attention_sound = pygame.mixer.Sound("sfx/attention.wav")
+game_start = pygame.mixer.Sound("sfx/game-start.wav")
+death_sound = pygame.mixer.Sound("sfx/death.wav")
+
 EGGS = {
     "Bulbasar": "img/bulbasaur-egg.png",
     "Jigglypuff": "img/jigglypuff-egg.png",
     "Charizard": "img/charizard-egg.png",
     "Squirtle": "img/squirtle-egg.png",
     "Ghastly": "img/ghastly-egg.png",
-    "Oddis": "img/oddish-egg.png"
+    "Oddish": "img/oddish-egg.png"
 }
 
 TAMAGOCHIS = {
@@ -13,7 +26,7 @@ TAMAGOCHIS = {
     "Charizard": "img/charizard.png",
     "Squirtle": "img/squirtle-egg.png",
     "Ghastly": "img/ghastly.png",
-    "Oddis": "img/oddish.png"
+    "Oddish": "img/oddish.png"
 }
 
 MENU_ITEMS = [
@@ -23,9 +36,51 @@ MENU_ITEMS = [
     {"name": "Play", "image": "img/play.png"}
 ]
 
-# TAMAGOTCHI STATS
+## TAMAGOTCHI STATS
 weight = 1
 happiness = 10
 health = 10
 sleep = 10
 
+class Tamagotchi(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(281, 318) ## WINDOW SIZE
+
+        self.bg_image = QPixmap("img/bg.png")
+        if self.bg_image.isNull():
+            print("Errorr")
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        ellipse_path = QPainterPath()
+        ellipse_path.addEllipse(QRectF(0,0,281,318))
+        
+        ## mask clipping for shell wallpaper
+        if not self.bg_image.isNull():
+            painter.setClipPath(QPainterPath())
+            bg_image = self.bg_image.scaled(281,318)
+            painter.drawPixmap(0,0, bg_image)
+        
+        pen = QPen(QColor(63,99,171))
+        pen.setWidth(8)
+        painter.setPen(pen)
+        painter.setClipping(False)
+        painter.drawEllipse(QRectF(0,0,281,318))
+
+        screen_w = 162
+        screen_h = 151
+        center_x = (self.width() - screen_w)//2
+        center_y = (self.height() - screen_h)//2
+        screen = QRectF(center_x, center_y, screen_w, screen_h)
+        painter.setBrush(QColor(200,200,200))
+        painter.drawRect(screen)
+
+
+#run
+app = QApplication(sys.argv)
+window = Tamagotchi()
+window.show()
+sys.exit(app.exec())
