0f8c3750a82a3ca372b8dae5db2dc5410cc34688
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-02-23 23:55:21 +0800

    added dicts/lists

---
+++ tamagotchi.py
@@ -0,0 +1,31 @@
+EGGS = {
+    "Bulbasar": "img/bulbasaur-egg.png",
+    "Jigglypuff": "img/jigglypuff-egg.png",
+    "Charizard": "img/charizard-egg.png",
+    "Squirtle": "img/squirtle-egg.png",
+    "Ghastly": "img/ghastly-egg.png",
+    "Oddis": "img/oddish-egg.png"
+}
+
+TAMAGOCHIS = {
+    "Bulbasaur": "img/bulbasaur.png",
+    "Jigglypuff": "img/jigglypuff.png",
+    "Charizard": "img/charizard.png",
+    "Squirtle": "img/squirtle-egg.png",
+    "Ghastly": "img/ghastly.png",
+    "Oddis": "img/oddish.png"
+}
+
+MENU_ITEMS = [
+    {"name": "Status", "image": "img/heart.png"},
+    {"name": "Medicine", "image": "img/medicine.png"},
+    {"name": "Feed", "image": "img/feed.png"},
+    {"name": "Play", "image": "img/play.png"}
+]
+
+# TAMAGOTCHI STATS
+weight = 1
+happiness = 10
+health = 10
+sleep = 10
+
